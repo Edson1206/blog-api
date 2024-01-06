a60c94b3990d5c474759038f7bf61d38a23783ed
Edson Pereira <ed.1206@gmail.com>
2024-01-06 01:46:30 -0300

    feat: :sparkles: Creating the Home and PostDetail pages

--- blog-api/src/pages/Home.jsx
+++ blog-api/src/pages/Home.jsx
@@ -1,9 +1,60 @@
+import { useEffect, useState } from "react"
+import fetchPosts from "../api/api";
+import { Container, TextField, Typography } from '@mui/material'
+import Pagination from "../components/Pagination";
+import BlogList from "../components/BlogList";
 
 const Home = () => {
+  const [posts, setPosts] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [postsPerPage] = useState(5);
+  const [searchTerm, setSearchTerm] = useState('');
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const data = await fetchPosts();
+        setPosts(data);
+      } catch (error) {
+        console.error('Error fetching submission:', error);
+      }
+    }
+    fetchData();
+  }, []);
+
+  const paginate = (pageNumber) => setCurrentPage(pageNumber);
+
+  const handleSearch = (event) => {
+    setSearchTerm(event.target.value);
+    setCurrentPage(1);
+  }
+
   return (
-    <div>
-      <p>Home</p>
-    </div>
+    <Container>
+      <Typography variant='h4' align='center' gutterBottom>
+        Blog Posts
+      </Typography>
+      <TextField
+        label='Search'
+        variant='outlined'
+        fullWidth
+        margin='normal'
+        value={searchTerm}
+        onChange={handleSearch}
+      />
+      <BlogList 
+        posts={posts} 
+        currentPage={currentPage} 
+        postsPerPage={postsPerPage} 
+        searchTerm={searchTerm} 
+      />
+      <Pagination 
+        postsPerPage={postsPerPage}
+        totalPosts={posts.length}
+        paginate={paginate}
+        currentPage={currentPage}
+      />
+    </Container>
   )
 }
 
--- blog-api/src/pages/PostDetail.jsx
+++ blog-api/src/pages/PostDetail.jsx
@@ -1,11 +1,50 @@
-
+import { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import { Container, Typography } from '@mui/material';
+import fetchPosts from '../api/api';
 
 const PostDetail = () => {
+  const { postId } = useParams();
+  const [post, setPost] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const data = await fetchPosts();
+        const selectedPost = data.find((item) => item.id === Number(postId));
+
+        if (selectedPost) {
+          setPost(selectedPost);
+        } else {
+          // Handle post not found
+        }
+      } catch (error) {
+        console.error('Error fetching submission:', error);
+      }
+    };
+
+    fetchData();
+  }, [postId]);
+
+  if (!post) {
+    // Handle loading state or post not found
+    return (
+      <Container>
+        <Typography variant="h4" align="center" gutterBottom>
+          Loading...
+        </Typography>
+      </Container>
+    );
+  }
+
   return (
-    <div>
-      <p>Home</p>
-    </div>
-  )
-}
+    <Container>
+      <Typography variant="h4" align="center" gutterBottom>
+        {post.title}
+      </Typography>
+      <Typography>{post.body}</Typography>
+    </Container>
+  );
+};
 
 export default PostDetail;
